62e6fa3a555113c03540b6dfb22802f9c204fc9b
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-10-25 12:39:18 +0200

    Created a simple password generator

---
+++ script.js
@@ -0,0 +1,18 @@
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var digits = "0123456789"
+
+var characters = lowercase + uppercase + digits;
+var charactersCount = characters.length;
+
+function generatePassword() {
+    var length = $("#length").val();
+    var password = '';
+
+    for (var i = 0; i < length; i++) {
+        var randomPos = Math.floor(Math.random() * charactersCount);
+        password += characters.charAt(randomPos);
+    }
+
+    $("#password").val(password);
+}
